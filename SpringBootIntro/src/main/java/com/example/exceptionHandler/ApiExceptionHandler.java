cb39f714f7d55e51a4795479227fc4f0b19f6f81
Henrique Rosa <henrique.prosa@hotmail.com>
2018-03-10 14:52:14 -0300

    Error: Internal error instead of Not Found in a not existing objectj deletion.

--- SpringBootIntro/src/main/java/com/example/exceptionHandler/ApiExceptionHandler.java
+++ SpringBootIntro/src/main/java/com/example/exceptionHandler/ApiExceptionHandler.java
@@ -11,6 +11,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -27,7 +31,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  * @author user
  */
 @ControllerAdvice // Notation to observe Exceptions for entire application
-public class IntroResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Autowired
     private MessageSource messageSource; // Spring Object to get the messages from messages.properties file.
@@ -61,6 +65,24 @@ public class IntroResponseEntityExceptionHandler extends ResponseEntityException
         List<Erro> errors = criateErrorList( ex.getBindingResult() );
         return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request); 
     }
+    
+    
+    @ExceptionHandler({EmptyResultDataAccessException.class})
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+        String mensagemUsuario = messageSource.getMessage("recurso.nao.existe", null, LocaleContextHolder.getLocale());
+        String mensagemDesenvolvedor = ex.getCause().toString();
+        List<Erro> errors = Arrays.asList( new Erro( mensagemUsuario, mensagemDesenvolvedor ) );
+        
+        return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+    
+    
+    @ExceptionHandler({InvalidDataAccessApiUsageException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void handleInvalidDataAccessApiUsageException(){
+    
+    }
+    
     
     
     
