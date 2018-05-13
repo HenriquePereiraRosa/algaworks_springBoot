69813a5afed566639a42fe033004b1ccb7c32074
Henrique Rosa <henrique.prosa@hotmail.com>
2018-05-13 13:12:08 -0300

    changed folder trying to solve heroku build error

---
+++ src/main/resources/db/migration/V003__criar_e_registrar_categorias.sql
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  User
+ * Created: Oct 18, 2017
+ */
+
+CREATE TABLE categoria (
+	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO categoria (nome) values ('Lazer');
+INSERT INTO categoria (nome) values ('Alimentação');
+INSERT INTO categoria (nome) values ('Supermercado');
+INSERT INTO categoria (nome) values ('Farmácia');
+INSERT INTO categoria (nome) values ('Outros');
