74f43f0112aa5fba646f161a11a9656c551719f2
Henrique Pereira Rosa <henrique.prosa@hotmail.com>
2018-07-21 17:12:49 -0300

    Generation of Statistical classes for graphics generation.

--- src/main/java/com/example/LancamentoEstatisticaCategoria.java
+++ src/main/java/com/example/LancamentoEstatisticaCategoria.java
@@ -5,10 +5,38 @@
  */
 package com.example;
 
+import com.example.model.Categoria;
+import java.math.BigDecimal;
+
 /**
  *
  * @author user
  */
 public class LancamentoEstatisticaCategoria {
+    
+    private Categoria categoria;
+    private BigDecimal total;
+
+    public LancamentoEstatisticaCategoria(Categoria categoria, BigDecimal total) {
+        this.categoria = categoria;
+        this.total = total;
+    }
+        
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+    
     
 }
--- src/main/java/com/example/repository/filter/LancamentoEstatisticaDia.java
+++ src/main/java/com/example/repository/filter/LancamentoEstatisticaDia.java
@@ -5,10 +5,47 @@
  */
 package com.example.repository.filter;
 
+import com.example.model.TipoLancamento;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 /**
  *
  * @author user
  */
 public class LancamentoEstatisticaDia {
-    
+    private TipoLancamento tipoLancamento;
+    private LocalDate dia;
+    private BigDecimal total;
+
+    public LancamentoEstatisticaDia(TipoLancamento tipoLancamento, LocalDate dia, BigDecimal total) {
+        this.tipoLancamento = tipoLancamento;
+        this.dia = dia;
+        this.total = total;
+    }
+
+    public TipoLancamento getTipoLancamento() {
+        return tipoLancamento;
+    }
+
+    public void setTipoLancamento(TipoLancamento tipoLancamento) {
+        this.tipoLancamento = tipoLancamento;
+    }
+
+    public LocalDate getDia() {
+        return dia;
+    }
+
+    public void setDia(LocalDate dia) {
+        this.dia = dia;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+        
 }
